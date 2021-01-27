4d8adbaffbb76893d7c7b8003a6509208407c253
Steve <stevepesce879@gmail.com>
2021-01-27 16:56:11 -0500

    list all recipes on index page

---
+++ src/main/java/spesce/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,7 @@
+package spesce.springframework.services;
+/** -------------------------------------------------------------
+ * Created By Steve P. on 1/27/2021 
+ * original package: spesce.springframework.services 
+ */
+public class RecipeServiceImpl {
+}
